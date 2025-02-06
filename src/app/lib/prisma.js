74f1001b15bf8341175fe06a55b3a69b832922e2
Esteban Gomez <unawaretub86@gmail.com>
2025-02-06 15:59:59 -0500

    changes

--- src/app/lib/prisma.js
+++ src/app/lib/prisma.js
@@ -1,5 +1,11 @@
-import { PrismaClient } from '@prisma/client';
+import { Pool } from 'pg'
+import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient();
+const connectionString = `${process.env.DATABASE_URL}`
 
-export default prisma;
+const pool = new Pool({ connectionString })
+const adapter = new PrismaPg(pool)
+const prisma = new PrismaClient({ adapter })
+
+export { prisma, pool }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './src/app/**/*.{js,ts,jsx,tsx}', // Escanea todos los archivos en src/app
+    './src/components/**/*.{js,ts,jsx,tsx}', // Escanea todos los archivos en src/components
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
